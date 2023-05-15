7188b59149adfcb74fab534fbe2069a6367c71eb
Konkyo <126088540+Konkyo@users.noreply.github.com>
2023-05-15 15:08:08 +0100

    Add files via upload

---
+++ Basic_JavaScript_Projects/Projec1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+var A = "Hello, World!"  //Assigning string value to the variable A
+window.alert(A);  //Display the value of A
+
+document.write("My brain said, \"It\'s confusing, take a break!\"<br>\"Get your brain cells working and force yourself to understand dumbass\"I responded." + "-K.C "); //Display a sting with line brak and concatenate it with K.C
+
+var firstName="Konrad";  //Assingn string value to firsName and lastName variables
+var lastName="Cwiklowski";
+
+var fullName=firstName + "" + lastName;  //Concatenate firsName and lastName variables and assign fullName
+
+document.write("FullName: " + fullName);  //Display the concatenated full name
+
+var a = 5;
+var b = 3;
+var c = 10;
+
+var result = (a + b) * c;
+
+document.write(result);
