33387b6b9e78bfae41e28874b6d45573b65e0bcf
Konkyo <126088540+Konkyo@users.noreply.github.com>
2023-05-16 13:14:22 +0100

    Add files via upload

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,5 +1,6 @@
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += "A lot from this book!";
+//function that uses the += operator
+function myFunction() { 
+    var sentence = "I am learning"; //Creating variable
+    sentence += "A lot from this book!"; //using += operator to add the variable
     document.getElementById("Concatenate").innerHTML = sentence;
 }
