28ca690daf0643330d5b2072064467ba685c1184
Konkyo <126088540+Konkyo@users.noreply.github.com>
2023-05-16 17:07:50 +0100

    Add files via upload

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -20,4 +20,40 @@ document.getElementById("result").innerHTML = "Large Number: " + largeNumber;
 
 // Display "-Infinity" using a negative number
 var negativeNumber = -1 / 0;
-document.getElementById("result_2").innerHTML += "<br>Negative Number: " + negativeNumber;
+document.getElementById("result_2").innerHTML += "<br>Negative Number: " + negativeNumber + "<br>" + "<br>";
+
+
+// Define variables
+var number1 = 5;
+var number2 = 10;
+
+// Use greater than operator to display true
+var result1 = number2 > number1;
+document.getElementById("result").innerHTML += "Result 1: " + result1 + "<br>";
+
+// Use lesser than operator to display false
+var result2 = number1 < number2;
+document.getElementById("result").innerHTML += "Result 2: " + result2 + "<br>";
+
+console.log(2 + 3);  // Addition operation
+
+console.log(false);
+
+console.log(2 == 2);  // true (both values are equal)
+console.log(2 == 3);  // false (values are not equal)
+
+console.log(2 === 2);  // true (same data type and value)
+console.log(2 === "2");  // false (different data type)
+console.log("2" === "2");  // true (same data type and value)
+console.log(true === 1);  // false (different data type and value)
+console.log(false === 0);  // false (different data type and value)
+
+console.log(true && true);   // true (both operands are truthy)
+console.log(false && false); // false (both operands are falulty)
+
+console.log(true && true);
+console.log(false && false);
+
+function notFunction() {
+    document.getElementById("Not").innerHTML= ! (20>10);
+}
