5dadd169ec85e6a263185cc56a733200fa3819cb
Konkyo <126088540+Konkyo@users.noreply.github.com>
2023-05-17 17:22:28 +0100

    Add files via upload

---
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,54 @@
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+function Call_String() {
+    let text = "Hello World";
+    let length = text.length;
+    document.getElementById("lenghtString").innerHTML = length;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y= 0; Y <Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] ="sleeping";
+    Cat_Picture[1] ="playing";
+    Cat_Picture[2] ="eating";
+    Cat_Picture[3] ="purring";
+    document.getElementById("Cat").innerHTML = "In this picture the cat is " +
+       Cat_Picture[2] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = " The cost of the " +
+       Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+
+
+function let_Assignment() {
+var x = 82;
+document.write(x);
+{
+    let X =33;
+    document.write("<br"> + X);
+}
+document.write("<br"> + x);
+}
